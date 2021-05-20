51bb7781af4e47feb79a8207da6f4850e4e7abcf
Fazle Rabbi <fazlerabbi_cse@hotmail.com>
2021-05-20 18:35:59 +0600

    create starting point for wpf project which start xels full node services, add --- Program.cs ,  add middleware to this file.

--- XelsDesktopWalletApp/App.xaml.cs
+++ XelsDesktopWalletApp/App.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Xels.Bitcoin;
-using Xels.Bitcoin.Base;
+
 using Xels.Bitcoin.Features.Wallet;
 using Xels.Bitcoin.Features.Wallet.Interfaces;
-using Xels.Bitcoin.Utilities;
 
 namespace XelsDesktopWalletApp
 {
@@ -23,35 +16,41 @@ namespace XelsDesktopWalletApp
         private readonly IHost _host;
         public App()
         {
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    ConfigureServices(services);
-                })
-                .Build();
+            //_host = Host.CreateDefaultBuilder()
+            //    .ConfigureServices((context, services) =>
+            //    {
+            //        ConfigureServices(services);
+            //    })
+            //    .Build();
         }
 
-        private void ConfigureServices(IServiceCollection services)
+        //private void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddSingleton<MainWindow>();
+        //    //services.AddSingleton<IFullNode, FullNode>();
+        //    //services.AddSingleton<IChainState, ChainState>();
+        //    //services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        //    //services.AddSingleton<IWalletManager, WalletManager>();
+
+        //}
+
+
+
+        //protected override async void OnStartup(StartupEventArgs e)
+        //{
+
+        //    await this._host.StartAsync();
+
+        //    var mainWindow = this._host.Services.GetRequiredService<MainWindow>();
+        //    mainWindow.Show();
+        //    base.OnStartup(e);
+
+        //}
+
+
+        public static void Entry(string[] args)
         {
-            services.AddSingleton<MainWindow>();
-            //services.AddSingleton<IFullNode, FullNode>();
-            //services.AddSingleton<IChainState, ChainState>();
-            //services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
-            services.AddSingleton<IWalletManager, WalletManager>();
-
-        }
-
-
-
-        protected override async void OnStartup(StartupEventArgs e)
-        {
-
-            await this._host.StartAsync();
-
-            var mainWindow = this._host.Services.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-            base.OnStartup(e);
-
+            
         }
 
     }
---
+++ XelsDesktopWalletApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using NBitcoin.Protocol;
+
+using Xels.Bitcoin;
+using Xels.Bitcoin.Builder;
+using Xels.Bitcoin.Configuration;
+using Xels.Bitcoin.Features.Api;
+using Xels.Bitcoin.Features.BlockStore;
+using Xels.Bitcoin.Features.ColdStaking;
+using Xels.Bitcoin.Features.Consensus;
+using Xels.Bitcoin.Features.MemoryPool;
+using Xels.Bitcoin.Features.Miner;
+using Xels.Bitcoin.Features.RPC;
+using Xels.Bitcoin.Features.SignalR;
+using Xels.Bitcoin.Features.SignalR.Broadcasters;
+using Xels.Bitcoin.Features.SignalR.Events;
+using Xels.Bitcoin.Networks;
+using Xels.Bitcoin.Utilities;
+using Xels.Features.SQLiteWalletRepository;
+
+namespace XelsDesktopWalletApp
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App application = new App();
+
+            try
+            {
+                //FreeConsole();
+                var nodeSettings = new NodeSettings(networksSelector: Networks.Xels,
+                    protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: args)
+                {
+                    MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                };
+
+                IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    //.UseWallet()
+                    //.UseBlockExplorer()
+                    .UsePosConsensus()
+                    .UseMempool()
+                    .UseColdStakingWallet()
+                    .AddSQLiteWalletRepository()
+                    .AddPowPosMining()
+                    .UseApi()
+                    .AddRPC();
+                //.UseDns()
+                //.UseDiagnosticFeature();
+
+                if (nodeSettings.EnableSignalR)
+                {
+                    nodeBuilder.AddSignalR(options =>
+                    {
+                        options.EventsToHandle = new[]
+                        {
+                            (IClientEvent) new BlockConnectedClientEvent(),
+                            new TransactionReceivedClientEvent()
+                        };
+
+                        options.ClientEventBroadcasters = new[]
+                        {
+                            (Broadcaster: typeof(StakingBroadcaster), ClientEventBroadcasterSettings: new ClientEventBroadcasterSettings
+                                {
+                                    BroadcastFrequencySeconds = 5
+                                }),
+                            (Broadcaster: typeof(WalletInfoBroadcaster), ClientEventBroadcasterSettings: new ClientEventBroadcasterSettings
+                                {
+                                    BroadcastFrequencySeconds = 5
+                                })
+                        };
+                    });
+                }
+
+                IFullNode node = nodeBuilder.Build();
+
+                if (node != null)
+                    _ = node.RunAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
