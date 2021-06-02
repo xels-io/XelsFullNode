c79eb2dfbcd5b6a75b92f00e5a28207121292b3f
Towsif <towsifpatwary@gmail.com>
2021-06-02 19:16:33 +0600

    Send windows - validations

--- XelsDesktopWalletApp/Views/SendSideChain.xaml.cs
+++ XelsDesktopWalletApp/Views/SendSideChain.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -56,6 +57,80 @@ namespace XelsDesktopWalletApp.Views
             this.walletName = walletname;
             this.walletInfo.walletName = this.walletName;
             LoadCreateAsync();
+        }
+
+        public bool isValid()
+        {
+            if (this.textMainchainFederationAddress.Text == string.Empty)
+            {
+                MessageBox.Show("An address is required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.textMainchainFederationAddress.Focus();
+                return false;
+            }
+
+            if (this.textMainchainFederationAddress.Text.Length > 25)
+            {
+                MessageBox.Show("An address is at least 26 characters long.");
+                this.textMainchainFederationAddress.Focus();
+                return false;
+            }
+            if (this.textSidechainDestinationAddress.Text == string.Empty)
+            {
+                MessageBox.Show("An address is required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.textSidechainDestinationAddress.Focus();
+                return false;
+            }
+
+            if (this.textSidechainDestinationAddress.Text.Length > 25)
+            {
+                MessageBox.Show("An address is at least 26 characters long.");
+                this.textSidechainDestinationAddress.Focus();
+                return false;
+            }
+
+            if (this.textAmount.Text == string.Empty)
+            {
+                MessageBox.Show("An amount is required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.textAmount.Focus();
+                return false;
+            }
+
+            if (this.textAmount.Text.Length > 0.00001)
+            {
+                MessageBox.Show("The amount has to be more or equal to 1.");
+                this.textAmount.Focus();
+                return false;
+            }
+
+            if (this.textAmount.Text.Length > 25)
+            {
+                MessageBox.Show("The total transaction amount exceeds your spendable balance.");
+                this.textAmount.Focus();
+                return false;
+            }
+
+            if (!Regex.IsMatch(this.textAmount.Text, @"^([0-9]+)?(\.[0-9]{0,8})?$"))
+            {
+                MessageBox.Show("Enter a valid transaction amount. Only positive numbers and no more than 8 decimals are allowed.");
+                this.textAmount.Focus();
+                return false;
+            }
+
+            if (this.textTransactionFee.Text == "")
+            {
+                MessageBox.Show("Transaction Fee is required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.textTransactionFee.Focus();
+                return false;
+            }
+
+            if (this.password.Password == "")
+            {
+                MessageBox.Show("Your password is required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.password.Focus();
+                return false;
+            }
+
+            return true;
         }
 
         public async void LoadCreateAsync()
@@ -144,19 +219,23 @@ namespace XelsDesktopWalletApp.Views
 
         private async Task SendTransactionSidechainAsync(string path)
         {
-            string postUrl = path + $"/wallet/send-transaction";
-            // var content = "";
-
-            HttpResponseMessage response = await client.PostAsync(postUrl, new StringContent(JsonConvert.SerializeObject(this.transactionSending.hex), Encoding.UTF8, "application/json"));
-
-
-            if (response.IsSuccessStatusCode)
+            if (isValid())
             {
-                // content = await response.Content.ReadAsStringAsync();
-            }
-            else
-            {
-                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+
+                string postUrl = path + $"/wallet/send-transaction";
+                // var content = "";
+
+                HttpResponseMessage response = await client.PostAsync(postUrl, new StringContent(JsonConvert.SerializeObject(this.transactionSending.hex), Encoding.UTF8, "application/json"));
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // content = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+                }
             }
 
         }
