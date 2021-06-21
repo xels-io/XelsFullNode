1d0c150b4ad9926ecb8ecb3d43616dcfc62727a1
Towsif <towsifpatwary@gmail.com>
2021-06-21 13:35:14 +0600

    added change from previous updated branch

---
+++ XelsDesktopWalletApp/Views/EthImport.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using XelsDesktopWalletApp.Models;
+
+namespace XelsDesktopWalletApp.Views
+{
+    /// <summary>
+    /// Interaction logic for EthImport.xaml
+    /// </summary>
+    public partial class EthImport : Window
+    {
+        private readonly WalletInfo walletInfo = new WalletInfo();
+        private string walletName;
+        public string WalletName
+        {
+            get
+            {
+                return this.walletName;
+            }
+            set
+            {
+                this.walletName = value;
+            }
+        }
+        public EthImport()
+        {
+            InitializeComponent();
+        }
+
+        public EthImport(string walletname)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            this.walletName = walletname;
+            this.walletInfo.walletName = this.walletName;
+
+        }
+
+
+        private void TransactionIDCopyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
