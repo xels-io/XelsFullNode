56f89248288b5959a28e6bd47035a80c6e97bfe0
Towsif <towsifpatwary@gmail.com>
2021-06-25 17:43:16 +0600

    Import Eth - Store, retrieve wallets in local file, get an wallet from local wallets.

--- XelsDesktopWalletApp/Common/CreateWallet.cs
+++ XelsDesktopWalletApp/Common/CreateWallet.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using NBitcoin;
+using System.Runtime.Caching;
+using System.IO;
 
 using Nethereum.Hex.HexConvertors.Extensions;
 using System.Threading.Tasks;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace XelsDesktopWalletApp.Common
 {
@@ -19,7 +23,8 @@ namespace XelsDesktopWalletApp.Common
 
     public class StoredWallet
     {
-        public Wallet Wallet { get; set; }
+        public string Address { get; set; }
+        public string PrivateKey { get; set; }
         public string Walletname { get; set; }
         public string Coin { get; set; }
         public string Wallethash { get; set; }
@@ -27,32 +32,33 @@ namespace XelsDesktopWalletApp.Common
 
     public class CreateWallet
     {
-        public Wallet WalletCreation(string mnemonic)
-        {
-            Wallet wallet = new Wallet();
+        List<StoredWallet> storedWallets = new List<StoredWallet>();
+        //public Wallet WalletCreation(string mnemonic)
+        //{
+        //    Wallet wallet = new Wallet();
 
-            //// creates new mnemonic only
-            //Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
-            //ExtKey hdRoot = mnemo.DeriveExtKey("my password");
-            //Console.WriteLine(mnemo);
+        //    //// creates new mnemonic only
+        //    //Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
+        //    //ExtKey hdRoot = mnemo.DeriveExtKey("my password");
+        //    //Console.WriteLine(mnemo);
 
-            // try2
-            Mnemonic mnemo = new Mnemonic("above turn genuine amused sister grocery fiscal draft wedding chat process prosper",
-                Wordlist.English);
-            ExtKey hdRoot = mnemo.DeriveExtKey("Shuvo912#");
+        //    // try2
+        //    Mnemonic mnemo = new Mnemonic("above turn genuine amused sister grocery fiscal draft wedding chat process prosper",
+        //        Wordlist.English);
+        //    ExtKey hdRoot = mnemo.DeriveExtKey("Shuvo912#");
 
 
-            //try3
-            var privateKey = "0000000000000000000000000000000000000000000000000000000000000001";
-            var account = new Account(privateKey);
+        //    //try3
+        //    var privateKey = "0000000000000000000000000000000000000000000000000000000000000001";
+        //    var account = new Account(privateKey);
 
-            wallet.PrivateKey = privateKey;
-            wallet.Address = account.Address;
+        //    wallet.PrivateKey = privateKey;
+        //    wallet.Address = account.Address;
 
-            return wallet;
-        }
+        //    return wallet;
+        //}
 
-        
+
         public Wallet WalletCreationFromPk(string pKey)
         {
             Wallet wallet = new Wallet();
@@ -64,16 +70,90 @@ namespace XelsDesktopWalletApp.Common
             return wallet;
         }
 
-        public void StoreLocally(Wallet wallet, string walletname, string coin, string wallethash)
+        public void StoreLocally(Wallet wallet, string walletname, string symbol, string wallethash)
         {
-            StoredWallet storedWallet = new StoredWallet();
-            storedWallet.Wallet.Address = wallet.Address;
-            storedWallet.Wallet.PrivateKey = wallet.PrivateKey;
-            storedWallet.Walletname = walletname;
-            storedWallet.Coin = coin;
-            storedWallet.Wallethash = wallethash;
+            if (walletname != null)
+            {
+                this.storedWallets = RetrieveWallets();
+
+                StoredWallet storedWallet = new StoredWallet();
+                storedWallet.Address = wallet.Address;
+                storedWallet.PrivateKey = wallet.PrivateKey;
+                storedWallet.Walletname = walletname;
+                storedWallet.Coin = symbol;
+                storedWallet.Wallethash = wallethash;
+
+                this.storedWallets.Add(storedWallet);
+                string JSONresult = JsonConvert.SerializeObject(this.storedWallets.ToArray(), Formatting.Indented);
+
+                string path = @"D:\Towsif\Projects\xels-fullnode-wpf-v3\XelsDesktopWalletApp\File\Wallets.json";
+
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+
+                    using (var sw = new StreamWriter(path, true))
+                    {
+                        sw.WriteLine(JSONresult.ToString());
+                        sw.Close();
+                    }
+                }
+                else if (!File.Exists(path))
+                {
+                    using (var sw = new StreamWriter(path, true))
+                    {
+                        sw.WriteLine(JSONresult.ToString());
+                        sw.Close();
+                    }
+                }
+
+
+            }
 
         }
+
+
+
+
+        public List<StoredWallet> RetrieveWallets()
+        {
+            using (StreamReader r = new StreamReader(@"D:\Towsif\Projects\xels-fullnode-wpf-v3\XelsDesktopWalletApp\File\Wallets.json"))
+            {
+                string json = r.ReadToEnd();
+                List<StoredWallet> wallets = new List<StoredWallet>();
+
+                if (json != null)
+                {
+                    wallets = JsonConvert.DeserializeObject<List<StoredWallet>>(json);
+                }
+
+                return wallets;
+            }
+        }
+
+        public StoredWallet GetLocalWallet(string walletname, string symbol)
+        {
+            StoredWallet wallet = new StoredWallet();
+
+            using (StreamReader r = new StreamReader(@"D:\Towsif\Projects\xels-fullnode-wpf-v3\XelsDesktopWalletApp\File\Wallets.json"))
+            {
+                string json = r.ReadToEnd();
+                List<StoredWallet> wallets = new List<StoredWallet>();
+
+                if (json != null)
+                {
+                    wallets = JsonConvert.DeserializeObject<List<StoredWallet>>(json);
+                }
+                wallet = wallets.Where(c => c.Walletname == walletname && c.Coin == symbol).FirstOrDefault();
+
+                return wallet;
+            }
+        }
+
+
+
+
 
 
     }
