aff8a8b2db6ed9a6f3a11a9e5d16b13658c57443
noyonmoududi <noyon.moududi@gmail.com>
2021-07-06 16:15:04 +0600

    Layout design page design for wpf app, test page check,meterial install for wpf from nuget

--- XelsDesktopWalletApp/Views/layout/MainLayout.xaml.cs
+++ XelsDesktopWalletApp/Views/layout/MainLayout.xaml.cs
@@ -22,18 +22,74 @@ namespace XelsDesktopWalletApp.Views.layout
             InitializeComponent();
         }
 
-       
-
-        private void MenuItem_ClickPage1(object sender, RoutedEventArgs e)
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
         {
-            this.page_Content.Children.Add(new Page1());
+            Application.Current.Shutdown();
         }
 
-        private void MenuItem_ClickPage2(object sender, RoutedEventArgs e)
+        private void GridBarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.page_Content.Children.Add(new Page2());
+            DragMove();
         }
 
-      
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            this.PageContent.Content = new Testpage();
+        }
+
+        //private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.ButtonCloseMenu.Visibility = Visibility.Visible;
+        //    this.ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        //    this.GridMain.Width = 880;
+        //}
+
+        //private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        //    this.ButtonOpenMenu.Visibility = Visibility.Visible;
+        //    this.GridMain.Width = 1010;
+
+        //}
+
+        //private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    //UserControl usc = null;
+        //    //GridMain.Children.Clear();
+
+        //    //switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+        //    //{
+        //    //    case "ItemHome":
+        //    //        usc = new UserControlHome();
+        //    //        GridMain.Children.Add(usc);
+        //    //        break;
+        //    //    case "ItemCreate":
+        //    //        usc = new UserControlCreate();
+        //    //        GridMain.Children.Add(usc);
+        //    //        break;
+        //    //    case "Page1":
+        //    //        GridMain.Children.Add(new Page1());
+        //    //        break;
+        //    //    default:
+        //    //        break;
+        //    //}
+
+        //    Page usc = null;
+        //    this.GridMain.Content = null;
+
+        //    switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+        //    {
+
+        //        case "Page1":
+        //            this.GridMain.Content = new Page1();
+        //            break;
+        //        case "Page2":
+        //            this.GridMain.Content = new Page2();
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
     }
 }
---
+++ XelsDesktopWalletApp/Views/layout/Testpage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XelsDesktopWalletApp.Views.layout
+{
+    /// <summary>
+    /// Interaction logic for Testpage.xaml
+    /// </summary>
+    public partial class Testpage : Page
+    {
+        public Testpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
